ab1439a1e6371ffed72621dd4196329d5433d8bc
deepeshjaindj <jaind1616@gmail.com>
2024-09-22 04:24:14 +0530

    Renamed Schema Files

--- src/app/schema/verifyCode.schema.ts
+++ src/app/schema/verifyCode.schema.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-export const verifySchema = z.object({
+export const verifyCodeSchema = z.object({
   code: z
     .string({ required_error: 'Code is required' })
     .length(6, 'Verification code must be 6 digits'),
